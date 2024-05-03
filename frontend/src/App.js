7441f95d2c140094dfa691fd5ae1baf079dee664
Aman <aman.mishra.2005@gmail.com>
2024-05-03 09:47:08 +0530

    Initiated Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { ArrowIcon, ChatIcon } from "./svg";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Aman
+      <ChatIcon className="red" />
+      <ArrowIcon className="red" />
     </div>
   );
-}
+};
 
 export default App;
