c310e68cc567daf42d5ba54f14ce5cb8f8599896
Aman <aman.mishra.2005@gmail.com>
2024-05-29 15:15:45 +0530

    Completed Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { logout } from "./features/userSlice";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home.js";
 import Login from "./pages/login.js";
 import Register from "./pages/register.js";
 import Error from "./components/Error.jsx";
+
 const App = () => {
   const { user } = useSelector((state) => state.user);
+  const { token } = user;
 
   return (
     <div className="dark">
