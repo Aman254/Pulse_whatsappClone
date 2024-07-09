95d7b7bb23b27d4ef95480089412a0b4e59b28b6
Aman <aman.mishra.2005@gmail.com>
2024-07-09 10:04:17 +0530

    Added a conversatin Service and also route to get all conversation

--- backend/src/services/conversation.service.js
+++ backend/src/services/conversation.service.js
@@ -1,5 +1,6 @@
 import createHttpError from "http-errors";
 import { ConversationModel, UserModel } from "../models/index.js";
+import logger from "../configs/logger.config.js";
 
 export const doesConversationExist = async (sender_id, receiver_id) => {
   let convos = await ConversationModel.find({
@@ -45,4 +46,28 @@ export const populateConversation = async (
     throw createHttpError.BadRequest("Oops.. Something went Wrong");
 
   return populatedConvo;
+};
+
+export const getUserConversations = async (user_id) => {
+  let conversations;
+  await ConversationModel.find({
+    users: { $elemMatch: { $eq: user_id } },
+  })
+    .populate("users", "-password")
+    .populate("admin", "-password")
+    .populate("lastestMessage")
+    .sort({ updatedAt: -1 })
+    .then(async (results) => {
+      results = await UserModel.populate(results, {
+        path: "latestMessage.sender",
+        select: "name email picture status",
+      });
+
+      conversations = results;
+    })
+    .catch((err) => {
+      console.error(err);
+      throw createHttpError.BadRequest("Something Went Wrong");
+    });
+  return conversations;
 };
