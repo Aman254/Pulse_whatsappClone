00fafd67e6852a4ef5c8e1b05f878777a58cea0f
Aman254 <aman.mishra.2005@gmail.com>
2024-08-01 13:28:54 +0530

    Fixed few Things

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,5 +1,5 @@
 import UserModel from "./user.model.js";
-import ConversationModel from "./conversationModel.js";
+import ConversationModel from "./ConversationModel.js";
 import MessageModel from "./messageModel.js";
 
 export { UserModel, ConversationModel, MessageModel };
